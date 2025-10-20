94c0f8ac2b46fd7992090248958b1aeb3e833a9a
Andrew Huang <k.andrew.huang@gmail.com>
2025-10-20 15:44:41 -0700

    Fix up

--- consensus/misc/migration/migration.go
+++ consensus/misc/migration/migration.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
@@ -32,6 +33,11 @@ func EnsureOptimismPredeploys(c *params.ChainConfig, blockNum *big.Int, db vm.St
 
 		// Create the account, set the code, nonce, balance, and storage.
 		for address, account := range alloc {
+			if !db.Empty(address) {
+				log.Warn("address already exists in state, skipping", "address", address)
+				continue
+			}
+
 			db.CreateAccount(address)
 			if len(account.Code) > 0 {
 				db.SetCode(address, account.Code)
